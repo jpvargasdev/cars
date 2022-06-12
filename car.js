a4c57ca286d00c5d2d206a8b82dc8a34091c43c7
jp-vargas <vargasm.jp@gmail.com>
2022-06-12 16:16:08 +0200

    first commit

---
+++ car.js
@@ -0,0 +1,135 @@
+class Car {
+  constructor(x, y, width, height, controlType, maxSpeed = 3) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+
+    this.maxSpeed = maxSpeed;
+    this.friction = 0.03;
+
+    this.angle = 0;
+    this.damaged = false;
+
+    if (controlType !== "DUMMY") {
+      this.sensors = new Sensor(this);
+    }
+    this.controls = new Controls(controlType);
+  }
+
+  update(roadBorders, traffic) {
+    this.#move();
+    this.polygon = this.#createPolygon();
+    this.damaged = this.#assessDamage(roadBorders, traffic);
+
+    if (this.sensors) {
+      this.sensors.update(roadBorders, traffic);
+    }
+  }
+
+  #assessDamage(roadBorders, traffic) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < traffic.length; i++) {
+      if (polysIntersect(this.polygon, traffic[i].polygon)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  #createPolygon() {
+    const points=[];
+    const rad=Math.hypot(this.width,this.height)/2;
+    const alpha=Math.atan2(this.width,this.height);
+    points.push({
+        x:this.x-Math.sin(this.angle-alpha)*rad,
+        y:this.y-Math.cos(this.angle-alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(this.angle+alpha)*rad,
+        y:this.y-Math.cos(this.angle+alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+        y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+        y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+    });
+    return points;
+  }
+
+  #move() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+
+    if (this.speed < -this.maxSpeed/2) {
+      this.speed = -this.maxSpeed/2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.x -= Math.sin(this.angle)*this.speed;
+    this.y -=Math.cos(this.angle)*this.speed;
+  }
+
+  draw(ctx, color) {
+    if (!this.polygon) return;
+
+    if (this.sensors) {
+      this.sensors.draw(ctx);
+    }
+    if (this.damaged) {
+      ctx.fillStyle= "gray";
+    } else {
+      ctx.fillStyle= color;
+    }
+    ctx.beginPath();
+    
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    ctx.lineTo(this.polygon[1].x, this.polygon[1].y);
+    ctx.lineTo(this.polygon[2].x, this.polygon[2].y);
+    ctx.lineTo(this.polygon[3].x, this.polygon[3].y);
+
+    ctx.fill();
+
+  }
+}
